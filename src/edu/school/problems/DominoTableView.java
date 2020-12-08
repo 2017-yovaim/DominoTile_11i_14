17af2b95bf659c7a35e0353e6abbaa2fd9ffd8c1
kids <kids@petia>
2020-12-08 11:18:40 +0200

    Changed DominoTableView again

--- src/edu/school/problems/DominoTableView.java
+++ src/edu/school/problems/DominoTableView.java
@@ -1,5 +1,7 @@
 package edu.school.problems;
 
+import java.awt.Graphics;
+
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,13 +10,14 @@ public class DominoTableView extends JComponent
 {
 	private DominoTable table = new DominoTable();
 	
-
-	public void paintComponent()
+	@Override
+	public void paintComponent(Graphics g)
 	{
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setBounds(0, 0, 1500, 800);
 		window.setVisible(true);
+		window.setLayout(null);
 		DominoTile[] table1 = this.table.getDominoTable();
 		for(int i = 0; i < table1.length && table1[i] != null; i++)
 		{
